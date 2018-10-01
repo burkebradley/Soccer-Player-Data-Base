b85a8824a83a212fb9d7ae6efaf0c11937e255be
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-10-01 14:55:24 -0600

    final commit

---
+++ SoccerPlayerDataBase/src/SoccerPlayerDataBase.java
@@ -0,0 +1,8 @@
+
+public class SoccerPlayerDataBase
+	{
+// Create an array list of players
+// Import text file of the all BPL players and their individual fifa stats
+// User will be able to search up player by name 
+// Will output all the players fifa stats and their nationality and professional game
+	}
